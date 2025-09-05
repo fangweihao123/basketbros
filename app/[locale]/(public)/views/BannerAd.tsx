6cc7f6b3bc4d041e8325cac6da855ef04a52f503
fangweihao123 <fwh19970312@gmail.com>
2025-09-05 12:11:03 +0800

    上传广告代码

--- app/[locale]/(public)/views/BannerAd.tsx
+++ app/[locale]/(public)/views/BannerAd.tsx
@@ -1,47 +1,33 @@
 'use client';
 
-import { useEffect } from 'react';
+import { JSX, useEffect, useRef } from 'react';
 
-export default function BannerAd() {
+export default function BannerAd() : JSX.Element {
+  const banner = useRef<HTMLDivElement>(null)
+
+  const atOptions = {
+    'key': 'faba8c51cc3e38ae5e238375752aec55',
+    'format': 'iframe',
+    'height': 250,
+    'width': 300,
+    'params': {}
+  };
+
   useEffect(() => {
-    // 设置广告配置
-    (window as any).atOptions = {
-      'key': 'faba8c51cc3e38ae5e238375752aec55',
-      'format': 'iframe',
-      'height': 250,
-      'width': 300,
-      'params': {}
-    };
+    if(banner.current && !banner.current.firstChild){
+      const conf = document.createElement('script')
+      const script = document.createElement('script')
+      script.type = 'text/javascript'
+      script.src = `//www.highperformancedformats.com/${atOptions.key}/invoke.js`
+      conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`
 
-    // 动态加载广告脚本
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = '//www.highperformanceformat.com/faba8c51cc3e38ae5e238375752aec55/invoke.js';
-    script.async = true;
-    
-    const adContainer = document.getElementById('banner-ad-top');
-    if (adContainer) {
-      adContainer.appendChild(script);
-    }
-
-    return () => {
-      // 清理脚本
-      if (adContainer && script) {
-        try {
-          adContainer.removeChild(script);
-        } catch (e) {
-          // 脚本可能已经被移除
-        }
+      if (banner.current) {
+          banner.current.append(conf)
+          banner.current.append(script)
       }
-    };
-  }, []);
+      console.log("banner" , banner)
+    }
+  }, [banner]);
 
-  return (
-    <div className="flex justify-center py-4 bg-gray-50 dark:bg-gray-900">
-      <div
-        id="banner-ad-top"
-        style={{ width: '300px', height: '250px' }}
-      />
-    </div>
-  );
+  return <div ref={banner}></div>;
 }
